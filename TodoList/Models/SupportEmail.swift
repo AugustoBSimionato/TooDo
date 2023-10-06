8009610415f2326a70370c3fada575deba95c0d6
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-10-06 11:56:11 -0300

    adding settings view

---
+++ TodoList/Models/SupportEmail.swift
@@ -0,0 +1,8 @@
+//
+//  SupportEmail.swift
+//  TodoList
+//
+//  Created by Augusto Simionato on 05/10/23.
+//
+
+import Foundation
---
+++ TodoList/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  TodoList
+//
+//  Created by Augusto Simionato on 05/10/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
